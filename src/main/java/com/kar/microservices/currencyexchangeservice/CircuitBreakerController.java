a904d8c5c9f149028b73ee941b78457157e8fa5b
vulpe kartik <vulpeekarthik@gmail.com>
2023-12-10 12:26:40 +0530

    currency-exchange

---
+++ src/main/java/com/kar/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,29 @@
+package com.kar.microservices.currencyexchangeservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@RestController
+public class CircuitBreakerController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	@GetMapping("/sample-api")
+	//@Retry(name="default")
+	@CircuitBreaker(name="default", fallbackMethod ="hardcodedResponse")
+	public String sampleApi() {
+		logger.info("sample Api call recieved");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		
+		return forEntity.getBody();
+	}
+	public String hardcodedResponse(Exception ex) {
+		return "fallback method";
+	}
+
+}
